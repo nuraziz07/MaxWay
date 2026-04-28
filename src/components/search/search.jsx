c798c9c5d549e668ed4de35a8aca80f004228702
NurazizCoder <nuralievnuraziz0@gmail.com>
2026-04-28 14:19:13 +0500

    o'zgarishlar

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -17,7 +17,7 @@ const Search = () => {
     return (
         <div
             className={
-                'sm:w-full bg-white z-2000 flex items-center justify-center fixed top-4'
+                'sm:w-full bg-white z-2000 flex items-center h-[10vh] justify-center fixed top-0'
             }
         >
             <div
